f7862dda9d156faebef114287f23392021091ae8
ItsWhoCares <itswhocaresman@gmail.com>
2024-08-09 12:34:33 +0530

    Updated Default time Zone

--- src/pages/api/getHistoricalData.js
+++ src/pages/api/getHistoricalData.js
@@ -1,4 +1,11 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Calcutta");
+
 function formatToChart(data, timePeriod) {
   //format to lables and data
   let chartData = {
